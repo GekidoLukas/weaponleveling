1c9573299cde8161c1c3ab9e2540a0a50edd067b
GekidoLukas <59490449+GekidoLukas@users.noreply.github.com>
2025-05-01 14:12:28 +0200

    New Mod Icon - Fixes

--- common/src/main/java/net/weaponleveling/WeaponLevelingMod.java
+++ common/src/main/java/net/weaponleveling/WeaponLevelingMod.java
@@ -1,18 +1,13 @@
 package net.weaponleveling;
 
-import com.google.common.base.Supplier;
-import com.google.common.base.Suppliers;
-import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.event.events.common.CommandRegistrationEvent;
 import dev.architectury.event.events.common.PlayerEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.registry.ReloadListenerRegistry;
-import dev.architectury.registry.registries.RegistrarManager;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.EnvType;
 import net.minecraft.server.packs.PackType;
 import net.weaponleveling.attribute.WLAttributes;
-import net.weaponleveling.client.ClientEvents;
 import net.weaponleveling.data.levelable_item.LevelableItemsLoader;
 import net.weaponleveling.data.mob_xp.MobXPLoader;
 import net.weaponleveling.item.ModItems;
@@ -21,12 +16,9 @@ import net.weaponleveling.server.command.ItemLevelCommand;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.HashMap;
-
 public class WeaponLevelingMod {
     public static final String MODID = "weaponleveling";
 
-    public static final Supplier<RegistrarManager> MANAGER = Suppliers.memoize(() -> RegistrarManager.get(MODID));
     public static final Logger LOGGER = LogManager.getLogger(MODID);
     public static void init() {
         WLAttributes.register();
@@ -45,18 +37,15 @@ public class WeaponLevelingMod {
                 }
             }
         }));
-//        PlayerEvent.PLAYER_QUIT.register(player -> {
-//
-//        });
 
 
 
-//        LifecycleEvent.SERVER_STARTED.register((LevelableDefaults::writeFromGameRule));
         //TODO
         //- Example Datapacks
-        //- Individual Editing of entities
         //- Fix remaining known bugs
-        //- Staring xp + following on next levels
+        //- Add Forge Item Tags
+        //- Toggleable Ranged Attribute
+        //- 
 
     }
 }
